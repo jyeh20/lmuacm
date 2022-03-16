7f3b7f10aad3087d63612e3df543a26e4f4ff3af
jyeh20 <jmyeh51@gmail.com>
2022-03-16 12:46:49 -0700

    hosting

--- webapp/src/firebase/firebase.js
+++ webapp/src/firebase/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBwHIqgRxcoL6uk-oaH2HmiFNxmDF6CGw8",
-  authDomain: "lmuacm-webapp.firebaseapp.com",
-  projectId: "lmuacm-webapp",
-  storageBucket: "lmuacm-webapp.appspot.com",
-  messagingSenderId: "207327897262",
-  appId: "1:207327897262:web:4479d0824aa747fe746f3f",
-  measurementId: "G-T3XDPQDCZW",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
