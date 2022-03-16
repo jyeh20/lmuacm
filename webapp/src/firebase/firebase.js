d20214286dbdada8b095a5fb2206a40e84d6eed7
jyeh20 <jmyeh51@gmail.com>
2022-03-16 13:05:17 -0700

    testing API key

--- webapp/src/firebase/firebase.js
+++ webapp/src/firebase/firebase.js
@@ -6,8 +6,6 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log(process.env.REACT_APP_TEST_API);
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
